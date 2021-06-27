dc1b06e297893b2a6566d53d3d417137e12ff7f5
Nuttawut Singhabut <big.nuttawut.s@gmail.com>
2021-06-27 18:06:03 +0700

    project

---
+++ src/main/java/com/bignuttawuts/springapi2021/SpringApi2021Application.java
@@ -0,0 +1,13 @@
+package com.bignuttawuts.springapi2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApi2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApi2021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bignuttawuts/springapi2021/SpringApi2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bignuttawuts.springapi2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApi2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
